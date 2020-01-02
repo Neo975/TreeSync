1a94b26eca181fe0e2477b2c5d4be347adbe006e
puvN <eugeniylukin@gmail.com>
2020-01-02 16:52:39 +0300

    version 2020-01-02

--- src/main/java/TItem.java
+++ src/main/java/TItem.java
@@ -1,11 +1,34 @@
 import java.io.File;
+import java.nio.file.Path;
 
 public class TItem extends File{
-	public TItem(String path) {
-		super(path);
+	private Path root;
+	private Path relative;
+	
+	public TItem(File file, File root) {
+		super(file.getPath());
+		this.root = root.toPath();
+		this.relative = this.root.relativize(file.toPath());
 	}
+	
+	public String getRelative() {
+		return relative.toString();
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if(other == null) {
+			return false;
+		}
+		if(!(other instanceof TItem)) {
+			return false;
+		}
 
-	public TItem(File file) {
-		super(file.getAbsolutePath());
+		return (this.getRelative()).equals(((TItem)other).getRelative());
+	}
+	
+	@Override
+	public int hashCode() {
+		return relative.hashCode();
 	}
 }
--- src/main/java/TSync.java
+++ src/main/java/TSync.java
@@ -1,7 +1,8 @@
 import java.io.File;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.nio.file.Path;
 
-//Commit 01.01.2020
 public class TSync {
     private static File FOLDER_ONE;
     private static File FOLDER_TWO;
@@ -9,18 +10,28 @@ public class TSync {
     private static HashSet<TItem> setTwo;
 
     public static void main(String[] args) {
-
-        checkArgs(args);
-        setOne = scanFolder(FOLDER_ONE);
+//		checkArgs(args);
+        checkArgs(new String[] {"C:\\temp2\\mike\\project\\UTA1", "C:\\temp2\\mike\\project\\UTA2"});
+//        checkArgs(new String[] {"C:\\Program Files", "C:\\Program Files (x86)"});
+        setOne = scanFolder(FOLDER_ONE, FOLDER_ONE);
         long startTime = System.nanoTime();
-        setTwo = scanFolder(FOLDER_TWO);
+        setTwo = scanFolder(FOLDER_TWO, FOLDER_TWO);
         long endTime = System.nanoTime();
         long duration = (endTime - startTime) / 1_000_000_000;
         System.out.println("duration: " + duration);   //in seconds
-        System.out.println("setOne's size: " + setOne.size());
-        System.out.println("setTwo's size: " + setTwo.size());
+		
+		testMethod();
+		HashSet<TItem> symmetricDifference = symmetricDifference();
+		HashSet<TItem> intersection = intersection();
 
-//		tempMethod();
+/*		
+		for(TItem item : symmetricDifference) {
+			System.out.println("Symmetric difference: " + item.getPath());
+		}
+*/
+		for(TItem item : intersection) {
+			System.out.println("Intersection: " + item.getPath());
+		}
     }
 
     private static void checkArgs(String[] args) {
@@ -42,31 +53,89 @@ public class TSync {
         }
     }
 
-    private static HashSet<TItem> scanFolder(File folder) {
+    private static HashSet<TItem> scanFolder(File folder, File root) {
         HashSet<TItem> finalSet = new HashSet<>();
-
-        try {
-            for (File child : folder.listFiles()) {
-                if (child.isDirectory()) {
-                    //recursive call
-                    finalSet.addAll(scanFolder(child));
-                } else {
-                    finalSet.add(new TItem(child));
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Problem file object: " + folder.getPath());
-            e.printStackTrace();
-        }
+		File[] childs = folder.listFiles();
+		
+		if(childs == null) {
+			System.out.println("Problem accessing folder. Check if you have access rights to this directory: \"" + folder.getPath() + "\"");
+			return finalSet;
+		}
+		for (File obj : childs) {
+			if (obj.isDirectory()) {
+				//recursive call
+				finalSet.addAll(scanFolder(obj, root));
+			} else {
+				finalSet.add(new TItem(obj, root));
+			}
+		}
 
         return finalSet;
     }
 	
-	private static void tempMethod() {
-		TItem item = new TItem("C:\\temp2\\mike\\project\\src\\main\\java");
-		File[] files = item.listFiles();
-		for(File f : files) {
-			System.out.println(f.getPath());
+	private static HashSet<TItem> symmetricDifference() {
+		HashSet<TItem> symDiff = new HashSet<>();
+		symDiff.addAll(setOne);
+		symDiff.addAll(setTwo);
+		symDiff.removeAll(intersection());
+		
+		return symDiff;
+	}
+	
+	private static HashSet<TItem> intersection() {
+		HashSet<TItem> intersection = new HashSet<>();
+		intersection.addAll(setOne);
+		intersection.retainAll(setTwo);
+		
+		return intersection;
+	}
+	
+	private static void testMethod() {
+/*		
+		File file1 = new File("C:\\temp2\\mike\\project\\UTA1\\CEUpdate.exe");
+		File file2 = new File("C:\\temp2\\mike\\project\\UTA2\\CEUpdate.exe");
+		TItem item1 = new TItem(file1, FOLDER_ONE);
+		TItem item2 = new TItem(file2, FOLDER_TWO);
+		HashSet<TItem> set1 = new HashSet<>();
+		HashSet<TItem> set2 = new HashSet<>();
+		set1.add(item1);
+		set2.add(item2);
+		boolean b = item1.equals(item2);
+		System.out.println(b);
+		System.out.println("item1.getRelative(): " + item1.getRelative());
+		System.out.println("item2.getRelative(): " + item2.getRelative());
+		
+		for(TItem item : set1) {
+			if(set2.contains(item)) {
+				System.out.println("Matched! " + item.getRelative());
+			}
 		}
+*/
+
+/*		
+        System.out.println("setOne's size: " + setOne.size());
+        System.out.println("setTwo's size: " + setTwo.size());
+		setOne.removeAll(setTwo);
+		System.out.println("Intersection");
+        System.out.println("setOne's size: " + setOne.size());
+        System.out.println("setTwo's size: " + setTwo.size());
+*/
+		
+/*		
+		System.out.println("setOne content:");
+		for(TItem item : setOne) {
+			System.out.println(item.getRelative());
+		}
+
+		System.out.println("setTwo content:");
+		for(TItem item : setTwo) {
+			System.out.println(item.getRelative());
+		}
+
+		boolean b = setOne.retainAll(setTwo);
+		System.out.println("b = " + b);
+        System.out.println("setOne's size: " + setOne.size());
+        System.out.println("setTwo's size: " + setTwo.size());
+*/
 	}
 }
